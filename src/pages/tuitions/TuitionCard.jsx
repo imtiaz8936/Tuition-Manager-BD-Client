1cbf9f81351722fa4a21a423577384b04230e48c
Imtiazul Hoque <imtiazulhoque8936@gmail.com>
2025-12-08 23:45:56 +0600

    showing tuitionCard functionality implemented

---
+++ src/pages/tuitions/TuitionCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router";
+
+const TuitionCard = ({ tuition }) => {
+  return (
+    <div className="bg-white border border-gray-200 rounded-xl p-5 shadow-sm hover:shadow-md transition">
+      {/* Subject */}
+      <h2 className="text-xl font-semibold text-gray-800 mb-1">
+        {tuition.subject}
+      </h2>
+
+      {/* Class */}
+      <p className="text-sm text-gray-600 mb-3">
+        Class: <span className="font-medium">{tuition.class}</span>
+      </p>
+
+      {/* Budget */}
+      <p className="text-gray-700 font-medium mb-2">
+        Budget:{" "}
+        <span className="text-lime-600 font-semibold">৳{tuition.budget}</span>
+      </p>
+
+      {/* Description */}
+      <p className="text-gray-700 text-sm mb-3">{tuition.description}</p>
+
+      {/* Location */}
+      <p className="text-gray-600 text-sm mb-3">
+        <span className="font-medium">Location:</span> {tuition.location}
+      </p>
+
+      {/* Contact (Static) */}
+      <p className="text-gray-700 font-medium mb-1">
+        Contact: <span className="text-blue-600">01700000000</span>
+      </p>
+
+      {/* Negotiable */}
+      <p className="text-green-600 font-semibold text-sm">Negotiable</p>
+
+      {/* Update & Delete Buttons */}
+      <div className="flex justify-between mt-5">
+        <Link>
+          <button className="bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium py-2 px-4 rounded-lg cursor-pointer transition-colors duration-200">
+            Update
+          </button>
+        </Link>
+
+        <button
+          // onClick={handleDeleteTuition}
+          className="bg-red-600 hover:bg-red-700 text-white text-sm font-medium py-2 px-4 rounded-lg cursor-pointer transition-colors duration-200"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TuitionCard;
--- src/pages/tuitions/Tuitions.jsx
+++ src/pages/tuitions/Tuitions.jsx
@@ -1,7 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { Link } from "react-router";
+import TuitionCard from "./TuitionCard";
 
 const Tuitions = () => {
-  return <div>Tuitions Page</div>;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: tuitions = [] } = useQuery({
+    queryKey: ["tuitions", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/tuitions?email=${user.email}`);
+      return res.data;
+    },
+  });
+
+  console.log(tuitions);
+
+  return (
+    <div className="w-full max-w-7xl mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {tuitions.map((tuition) => (
+          <TuitionCard key={tuition._id} tuition={tuition}></TuitionCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tuitions;
